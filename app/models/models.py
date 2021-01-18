345a3644063192331883928c3ba53ae507c21679
john <jgh2102@gmail.com>
2021-01-18 11:40:26 +0900

    added more tables

--- app/models/models.py
+++ app/models/models.py
@@ -10,6 +10,20 @@ grocery_users = db.Table(
   db.Column("user_id", db.Integer, db.ForeignKey("users.id"), primary_key=True),
 )
 
+freezer_grocery_users = db.Table(
+  "freezer_grocery_users",
+  db.Model.metadata,
+  db.Column("freezer_grocery_id", db.Integer, db.ForeignKey("freezer_groceries.id"), primary_key=True),
+  db.Column("user_id", db.Integer, db.ForeignKey("users.id"), primary_key=True),
+)
+
+pantry_grocery_users = db.Table(
+  "pantry_grocery_users",
+  db.Model.metadata,
+  db.Column("pantry_grocery_id", db.Integer, db.ForeignKey("pantry_groceries.id"), primary_key=True),
+  db.Column("user_id", db.Integer, db.ForeignKey("users.id"), primary_key=True),
+)
+
 
 class GroceryType(db.Model):
     __tablename__ = "grocery_types"
@@ -69,6 +83,8 @@ class User(db.Model, UserMixin):
     avatar = db.Column(db.String(255))
 
     groceries = db.relationship("Grocery", secondary=grocery_users, back_populates="users")
+    freezer_groceries = db.relationship("FreezerGrocery", secondary=freezer_grocery_users, back_populates="users")
+    pantry_groceries = db.relationship("PantryGrocery", secondary=pantry_grocery_users, back_populates="users")
 
     @property
     def password(self):
@@ -92,4 +108,98 @@ class User(db.Model, UserMixin):
           "country": self.country,
           "avatar": self.avatar,
           "email": self.email
+        }
+
+## beginning of ecofridge 2.0 expansion of freezer----------------------------------------------
+
+class FreezerGroceryType(db.Model):
+    __tablename__ = "freezer_grocery_types"
+
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String(255), nullable=False, unique=True)
+    image = db.Column(db.String(255))
+    days_to_expiry = db.Column(db.Integer, nullable=False)
+    # grocery = db.relationship("Grocery", back_populates="type")
+    def to_dict(self):
+        return {
+          "id": self.id,
+          "type": self.type,
+          "image": self.image,
+          "days_to_expiry": self.days_to_expiry,
+        }
+
+class FreezerGrocery(db.Model):
+    __tablename__ = "freezer_groceries"
+
+    id = db.Column(db.Integer, primary_key=True)
+    item_name = db.Column(db.String(50), nullable=False)
+    freezer_grocery_types_id = db.Column(db.Integer, db.ForeignKey("freezer_grocery_types.id"), nullable=True)
+    createdAt = db.Column(db.DateTime, server_default=db.func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable = False)
+
+    type = db.relationship("FreezerGroceryType")
+    users = db.relationship("User", secondary=freezer_grocery_users, back_populates="freezer_groceries")
+
+    def to_dict(self):
+        return {
+          "id": self.id,
+          "item_name": self.item_name,
+          "freezer_grocery_types_id": self.freezer_grocery_types_id,
+          "createdAt": self.createdAt,
+        }
+
+    def to_type_dict(self):
+        return {
+          "id": self.id,
+          "item_name": self.item_name,
+          "freezer_grocery_types_id": self.freezer_grocery_types_id,
+          "createdAt": self.createdAt,
+          "type": self.type.to_dict()
+        }
+
+
+## beginning of ecofridge 2.0 expansion of pantry----------------------------------------------
+class PantryGroceryType(db.Model):
+    __tablename__ = "pantry_grocery_types"
+
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String(255), nullable=False, unique=True)
+    image = db.Column(db.String(255))
+    days_to_expiry = db.Column(db.Integer, nullable=False)
+    # grocery = db.relationship("Grocery", back_populates="type")
+    def to_dict(self):
+        return {
+          "id": self.id,
+          "type": self.type,
+          "image": self.image,
+          "days_to_expiry": self.days_to_expiry,
+        }
+
+class PantryGrocery(db.Model):
+    __tablename__ = "pantry_groceries"
+
+    id = db.Column(db.Integer, primary_key=True)
+    item_name = db.Column(db.String(50), nullable=False)
+    pantry_grocery_types_id = db.Column(db.Integer, db.ForeignKey("pantry_grocery_types.id"), nullable=True)
+    createdAt = db.Column(db.DateTime, server_default=db.func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable = False)
+
+    type = db.relationship("PantryGroceryType")
+    users = db.relationship("User", secondary=pantry_grocery_users, back_populates="pantry_groceries")
+
+    def to_dict(self):
+        return {
+          "id": self.id,
+          "item_name": self.item_name,
+          "pantry_grocery_types_id": self.pantry_grocery_types_id,
+          "createdAt": self.createdAt,
+        }
+
+    def to_type_dict(self):
+        return {
+          "id": self.id,
+          "item_name": self.item_name,
+          "pantry_grocery_types_id": self.pantry_grocery_types_id,
+          "createdAt": self.createdAt,
+          "type": self.type.to_dict()
         }
