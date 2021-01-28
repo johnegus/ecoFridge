239567f139e25cbb8e70eb2660ed59b0ef617855
john <jgh2102@gmail.com>
2021-01-29 07:05:04 +0900

    changed loader and recipe pic animation

--- react-app/src/components/dashboard/Dashboard.js
+++ react-app/src/components/dashboard/Dashboard.js
@@ -246,7 +246,7 @@ const pieOptions= {
       <>
     
       <main className="centered middled">
-      <img className='icon-progress' height='50px' width='50px' src={fridgeIcon} alt='fridgeIcon' />
+      <img className='icon-progress' height='75px' width='150px' src={fridgeIcon} alt='fridgeIcon' />
         <b>Loading Groceries...</b>
         
         <div className='circle-progress'> <CircularProgress size='100px' color='secondary'/> </div>
